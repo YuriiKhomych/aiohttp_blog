72f4213bf3d04e19e1569237e1ff503bfebfcd0f
yuriikhomych <yuriykhomich@gmail.com>
2020-03-22 14:59:22 +0200

    Create base application

---
+++ aiohttpdemo_blog/routes.py
@@ -0,0 +1,12 @@
+from aiohttpdemo_blog.views import index, login, logout, create_post, sign_up
+
+
+def setup_routes(app):
+    app.router.add_get('/', index, name='index')
+    app.router.add_get('/sign_up', sign_up, name='sign_up')
+    app.router.add_post('/sign_up', sign_up, name='sign_up')
+    app.router.add_get('/login', login, name='login')
+    app.router.add_post('/login', login, name='login')
+    app.router.add_get('/logout', logout, name='logout')
+    app.router.add_get('/create', create_post, name='create-post')
+    app.router.add_post('/create', create_post, name='create-post')
